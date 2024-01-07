9733e7b2d7d64956ad4addd7c1a2b76f9da22fa9
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-01-07 23:09:33 +0100

    Prepare for 0.04 version

--- CSS.pm
+++ CSS.pm
@@ -15,7 +15,7 @@ Readonly::Array our @ABSOLUTE_LENGTHS => qw(cm mm in px pt pc);
 Readonly::Array our @RELATIVE_LENGTHS => qw(em ex ch rem vw vh vmin vmax %);
 Readonly::Array our @COLOR_FUNC => qw(rgb rgba hsl hsla);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub check_array_css_color {
 	my ($self, $key) = @_;
@@ -559,6 +559,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
