85387b1f66279a28f92eab9716906bd2268919e0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-18 09:12:57 +0200

    Prepare for 0.09 version

--- CSS.pm
+++ CSS.pm
@@ -19,7 +19,7 @@ Readonly::Array our @BORDER_WIDTHS => qw(thin medium thick);
 Readonly::Array our @RELATIVE_LENGTHS => qw(em ex ch rem vw vh vmin vmax %);
 Readonly::Array our @COLOR_FUNC => qw(rgb rgba hsl hsla);
 
-our $VERSION = 0.08;
+our $VERSION = 0.09;
 
 sub check_array_css_color {
 	my ($self, $key) = @_;
@@ -729,6 +729,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.08
+0.09
 
 =cut
